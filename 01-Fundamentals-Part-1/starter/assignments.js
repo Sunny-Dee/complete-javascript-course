76f58e922368bd7a2820af01b5b8a71428aab786
Deliana Escobari <Sunny-Dee@users.noreply.github.com>
2025-04-17 01:02:55 +0000

    Add script to the html body

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,8 @@
+let country = 'Bolivia';
+let continent = 'South America';
+var population = 10000000;
+
+// console.log(`The country of ${country} (in ${continent}) has ${population.toLocaleString()} peoples`)
+
+console.log(country);
+console.log(continent);
